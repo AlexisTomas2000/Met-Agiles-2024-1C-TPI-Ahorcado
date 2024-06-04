93587bbfb71a00f50b79c555bb0cb59e76ad4290
AlexisTomas2000 <alexisjosetomas@gmail.com>
2024-06-03 21:59:21 -0300

    1er TDD

---
+++ ahorcado.py
@@ -0,0 +1,7 @@
+class Ahorcado():
+    pass
+    def __init__(self) -> None:
+        self.nombre = ''
+  
+    def login(self,name:str):
+        self.nombre = name
---
+++ test_ahorcado.py
@@ -0,0 +1,8 @@
+from ahorcado import *
+
+nombre = 'Alexis'
+
+def test_log():
+    jugada = Ahorcado()
+    jugada.login(nombre)=='Alexis'
+    
