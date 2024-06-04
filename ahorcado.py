873683ff344eab21fdbf82be2d8eb3b673c5ef6e
AlexisTomas2000 <alexisjosetomas@gmail.com>
2024-06-03 22:11:18 -0300

    2do TDD

--- ahorcado.py
+++ ahorcado.py
@@ -2,6 +2,10 @@ class Ahorcado():
     pass
     def __init__(self) -> None:
         self.nombre = ''
+        self.letra=''
   
     def login(self,name:str):
         self.nombre = name
+
+    def jugar(self,leta:chr):
+        self.letra=leta
--- test_ahorcado.py
+++ test_ahorcado.py
@@ -5,4 +5,7 @@ nombre = 'Alexis'
 def test_log():
     jugada = Ahorcado()
     jugada.login(nombre)=='Alexis'
+def test_letra():
+    jugada = Ahorcado()
+    jugada.jugar('A')=='A'
     
